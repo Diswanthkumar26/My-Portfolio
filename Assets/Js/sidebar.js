5b6434f71ef287a4d999c81b6d89b1e588427fee
Diswanthkumar26 <diswanthkumar.ch@gmail.com>
2025-01-27 10:33:14 +0530

    changes

--- Assets/Js/sidebar.js
+++ Assets/Js/sidebar.js
@@ -10,3 +10,27 @@ function hideSidebar() {
     document.getElementById("sidebar").style.transition = "transform 0.3s ease-in-out"; 
 }
 
+// =======================================================================================================================================================
+
+// uparrow 
+
+let calcscrollValue = () => {
+    let scrollProgress = document.getElementById("scroll-Arrow");
+    let progressValue = document.getElementById("uparrow");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    
+    if(pos>100){
+        scrollProgress.style.display = "grid";
+    }
+    else{
+        scrollProgress.style.display= "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#ffbf00 ${scrollValue}%, whitesmoke ${scrollValue}%)`;
+}
+window.onscroll = calcscrollValue;
+window.onload = calcscrollValue;
